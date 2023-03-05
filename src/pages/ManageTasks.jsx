6630814e8153623040c6e5234b52cf3216fade48
Hanad5670 <hanad5670@gmail.com>
2023-03-05 08:51:06 -0500

    Updated task creation box with relevant data.

--- src/pages/ManageTasks.jsx
+++ src/pages/ManageTasks.jsx
@@ -1,30 +1,29 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { KanbanComponent, ColumnsDirective, ColumnDirective, Kanban } from '@syncfusion/ej2-react-kanban';
 
-import { kanbanGrid } from '../data/dummy';
-import { Header } from '../components';
-import { getTeamTasks } from '../data/dummy';
-
 import { DialogComponent } from '@syncfusion/ej2-react-popups';
+import { kanbanGrid, getTeamTasks } from '../data/dummy';
+import { Header } from '../components';
+
 import TaskForm from '../components/TaskForm';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const ManageTasks = () => {
-  const [kanbanData, setKanbanData] = useState()
-  const {user} = useStateContext();
+  const [kanbanData, setKanbanData] = useState();
+  const { user } = useStateContext();
 
   useEffect(() => {
     getTeamTasks(user)
       .then((res) => {
         const data = res.map((item, i) => {
-          let status = item.status;
+          let { status } = item;
           if (item.status === 'Completed') {
             status = 'Close';
-          }else if(item.status === 'InReview'){
+          } else if (item.status === 'InReview') {
             status = 'Testing';
-          }else if(item.status === 'Inprogress'){
+          } else if (item.status === 'Inprogress') {
             status = 'InProgress';
-          }else if(item.status === 'ToDo'){
+          } else if (item.status === 'ToDo') {
             status = 'Open';
           }
 
@@ -35,45 +34,47 @@ const ManageTasks = () => {
             Summary: item.description,
             Priority: item.priority,
             Assignee: item.assignedTo.firstName,
-           }
-         })
-         setKanbanData(data)
+          };
+        });
+        setKanbanData(data);
       })
       .catch((err) => console.log(err));
   }, []);
 
   const [boxStatus, setBoxStatus] = React.useState({ hideDialog: false });
-           function onOverlayClick() {
-              setBoxStatus({ hideDialog: false });
-           }
-           function dialogClose() {
-              setBoxStatus({ hideDialog: false });
-           }
-           function handleClick() {
-             setBoxStatus({ hideDialog: true });
+  function onOverlayClick() {
+    setBoxStatus({ hideDialog: false });
   }
-  
-  return (<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="App" title="Kanban" />
-    <div className="App" id="dialog-target">
+  function dialogClose() {
+    setBoxStatus({ hideDialog: false });
+  }
+  function handleClick() {
+    setBoxStatus({ hideDialog: true });
+  }
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      <div className="App" id="dialog-target">
         {/* eslint-disable-next-line jsx-a11y/no-redundant-roles,react/button-has-type */}
         <button className="e-control e-btn" id="targetButton1" role="button" onClick={handleClick.bind(this)}>Open</button>
         {/* eslint-disable-next-line react/jsx-no-bind */}
         <DialogComponent content={TaskForm} isModal target="#dialog-target" visible={boxStatus.hideDialog} close={dialogClose} overlayClick={onOverlayClick} />
+      </div>
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        swimlaneSettings={{ keyField: 'Assignee' }}
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
-    <KanbanComponent
-      id="kanban"
-      keyField="Status"
-      swimlaneSettings={{ keyField: 'Assignee' }}
-      dataSource={kanbanData}
-      cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-    >
-      <ColumnsDirective>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-      </ColumnsDirective>
-    </KanbanComponent>
-  </div>)
+  );
 };
 
 export default ManageTasks;
